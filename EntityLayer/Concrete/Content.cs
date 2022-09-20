09f629003d0d00b6576baff87ea4322d45ce4410
Ahmet Usta <108552711+AhmetUstaa@users.noreply.github.com>
2022-09-20 16:48:10 +0300

    5. ders sonrası

---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Content
+    {
+        public int Content { get; set; }
+        public string ContentValue { get; set; }
+        public  DateTime ContentDate { get; set; }
+        //ContentWriter
+        //ContentHeader
+
+
+    }
+}
---
+++ EntityLayer/Concrete/Writer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Writer
+    {
+        public int WriterID { get; set; }
+        public int WriterName { get; set; }
+        public int WriterSurName { get; set; }
+        public int WriterImage { get; set; }
+        public int WriterMail { get; set; }
+        public int WriterPassword { get; set; }
+
+    }
+}
