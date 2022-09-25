e632debf3eae555a56d205d90e81f2abefaeb422
Ahmet Usta <108552711+AhmetUstaa@users.noreply.github.com>
2022-09-25 12:32:29 +0300

    Generic Repository added.

---
+++ DataAccessLayer/Concrete/Repositories/Generic Reporsitory.cs
@@ -0,0 +1,48 @@
+﻿using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class Generic_Reporsitory<T> : IRepository<T> where T : class
+    {
+        Context c = new Context();
+        DbSet<T> _object;
+
+        public Generic_Reporsitory()
+        {
+            _object = c.Set<T>();
+        }
+        public void Delete(T p)
+        {
+            _object.Remove(p);
+            c.SaveChanges();
+        }
+
+        public void Insert(T p)
+        {
+            _object.Remove(p);
+            c.SaveChanges();
+        }
+
+        public List<T> List()
+        {
+            return _object.ToList();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> filter)
+        {
+            return _object.Where(filter).ToList();
+        }
+
+        public void Update(T p)
+        {
+            c.SaveChanges();
+        }
+    }
+}
