f5e540c4252ad0a882cdab3ae3c3e9e59a9ae425
Ahmet Usta <108552711+AhmetUstaa@users.noreply.github.com>
2022-09-29 22:17:45 +0300

    SideBar

---
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EFCategoryDal());
+        public ActionResult Index()
+        {
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidatior categoryvalidator = new CategoryValidatior();
+            ValidationResult result = categoryvalidator.Validate(p);
+            if (result.IsValid) 
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
